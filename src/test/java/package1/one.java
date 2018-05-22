5bca3beb67d2b98f812b07fb05fb7093b2c578f2
Your Name <you@example.com>
2018-05-22 15:29:47 +0530

    this is my third addition in git repo

---
+++ src/test/java/package1/one.java
@@ -0,0 +1,19 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class one {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("hi");
+	}
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("helo");
+	}
+
+}
